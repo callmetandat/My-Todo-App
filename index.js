2a9dc6156baf4150b270c2888ea6a4d3c019cdbe
callmetandat <letandat15082002@gmail.com>
2025-07-15 17:00:19 +0700

    Add CD pipeline for Docker

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Node.js 20 + Docker 1234');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+//
+//
