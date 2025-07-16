04eed05027be6f79227f3224752254a786d87560
callmetandat <letandat15082002@gmail.com>
2025-07-17 03:57:53 +0700

    Trigger Github Actions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const PORT = 4567;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Node.js 20 + Docker \nYou see this message that means CI run before CD');
+  res.end('Hello from Node.js 20 + Docker \nYou see this message that means CI run before CD!');
 });
 
 server.listen(PORT, () => {
